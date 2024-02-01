fb9e7af8538e27ab680b6a14668d6849e47e9038
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-01 11:43:53 +0300

    Coordinator seeder

---
+++ database/seeders/CoordinatorSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CoordinatorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory()->create();
+
+        $user->assignRole(Role::COORDINATOR);
+    }
+}
