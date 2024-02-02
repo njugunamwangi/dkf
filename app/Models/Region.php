3bfbadb4d51927fa33d70ccf4cb5bef91a8ba648
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-02 09:36:44 +0300

    Members model

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Region extends Model
@@ -16,5 +17,9 @@ class Region extends Model
 
     public function coordinator(): BelongsTo {
         return $this->belongsTo(User::class, 'coordinator_id', 'id');
+    }
+
+    public function members() : HasMany {
+        return $this->hasMany(User::class);
     }
 }
