bd31f687c62a09a94f0d001fd67fcb327e41668f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-02 14:10:44 +0300

    S Mwangi's AT

---
+++ config/africastalking.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'username' => env('AT_USERNAME', 'sandbox'),
+    'api-key' => env('AT_KEY'),
+    'sms' => [
+        'from' => env('AT_KEY'),
+    ],
+    'payment' => [
+        'product-name' => env('AFRICASTALKING_PAYMENT_PRODUCT'),
+    ],
+    'voice' => [
+        'from' => env('AFRICASTALKING_VOICE_PHONE_NUMBER'),
+    ]
+];
