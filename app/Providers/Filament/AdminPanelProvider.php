6786e0b9055046a7cb5bad2a21ca2d3be1623c90
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-02 10:17:30 +0300

    Navigation groups

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugi
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -42,6 +43,10 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make('Staff')->icon('heroicon-o-users'),
+                NavigationGroup::make('Members Management')->icon('heroicon-o-user-group'),
             ])
             ->middleware([
                 EncryptCookies::class,
