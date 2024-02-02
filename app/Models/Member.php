06bd178e571f68be7a73095dc8468b33034442e4
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-02 14:13:39 +0300

    Member notification

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -6,15 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Member extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Notifiable;
 
     protected $guarded = [];
 
     public function region() : BelongsTo {
         return $this->belongsTo(Region::class);
+    }
+
+    public function routeNotificationForAfricasTalking($notification)
+    {
+        return $this->phone_number;
     }
 }
