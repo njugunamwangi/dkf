a832ca5f902aac5e006d9b57208d7bdbcf2b6342
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-02 14:12:24 +0300

    Communication controller

---
+++ app/Http/Controllers/CommunicationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Member;
+use App\Models\Region;
+use Illuminate\Http\Request;
+
+class CommunicationController extends Controller
+{
+    public function index() {
+        $user = auth()->user();
+        $region = Region::where('coordinator_id', '=', $user->id)->first();
+
+        $members = Member::query()
+            ->where('region_id', '=', $region->id)
+            ->get();
+
+        return view('communication', compact('members'));
+    }
+}
