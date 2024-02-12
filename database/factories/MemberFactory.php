f461c8e6ffff29af2a9fb2fdf3d9e84d34164196
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-12 10:07:12 +0300

    Members factory

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Region;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,11 @@ class MemberFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'phone_number' => $this->faker->e164PhoneNumber(),
+            'id_number' => rand(10000000, 99999999),
+            'entry_number' => rand(1000, 10000),
+            'region_id' => $this->faker->randomElement(Region::all()->pluck('id')),
         ];
     }
 }
