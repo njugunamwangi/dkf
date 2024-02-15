0cefcb6b0f09c35ee18bf5be7ceda705efe0390c
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-15 17:02:48 +0300

    Members exporter

---
+++ app/Filament/Exports/MemberExporter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\Member;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+
+class MemberExporter extends Exporter
+{
+    protected static ?string $model = Member::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('id')
+                ->label('ID'),
+            ExportColumn::make('region.id'),
+            ExportColumn::make('name'),
+            ExportColumn::make('phone_number'),
+            ExportColumn::make('id_number'),
+            ExportColumn::make('entry_number'),
+            ExportColumn::make('deleted_at'),
+            ExportColumn::make('created_at'),
+            ExportColumn::make('updated_at'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your member export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}
