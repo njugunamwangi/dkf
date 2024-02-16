5316b2827e936416fc4dd337b2e98f33fe17678a
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-16 09:40:03 +0300

    Project model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use SamuelMwangiW\Africastalking\Facades\Africastalking;
@@ -19,6 +20,10 @@ class Member extends Model
 
     public function region() : BelongsTo {
         return $this->belongsTo(Region::class);
+    }
+
+    public function projects(): BelongsToMany {
+        return $this->belongsToMany(Project::class);
     }
 
     public function routeNotificationForAfricasTalking($notification)
