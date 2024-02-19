b8e784818c6747db6831ffc69bc7a26a7b06dbe2
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-19 17:01:37 +0300

    Member profile

---
+++ database/migrations/2024_02_19_162751_create_member_profiles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Member;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('member_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Member::class)->constrained()->cascadeOnDelete();
+            $table->integer('shares');
+            $table->string('entry_number')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('member_profiles');
+    }
+};
